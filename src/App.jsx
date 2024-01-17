49d2d198e9c8c09215061d7bff8c6981cf037ac2
Joseph <jq84144@gmail.com>
2024-01-17 03:51:47 -0500

    About me page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // PAGES
 import NotFound from './pages/NotFound'
 import Home from './pages/Home'
+import About from './pages/About'
 import Contact from './pages/Contact'
 
 // COMPONENTS
@@ -20,6 +21,7 @@ function App () {
         <Routes>
           <Route path='*' element={<NotFound />} />
           <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
           <Route path='/contact' element={<Contact />} />
         </Routes>
       </BrowserRouter>
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,54 @@
+// MODULES
+import { Link } from 'react-router-dom'
+
+// STYLES
+import './style/About.scss'
+import { FaReact, FaJs, FaSass } from 'react-icons/fa6'
+
 function About () {
-  return <div>About</div>
+  return (
+    <div className='About'>
+      <div className='wrap'>
+        <header className='top'>
+          <section>
+            <header>
+              <h2>Hi, my name is</h2>
+              <h1>Joseph Quiroz</h1>
+            </header>
+            <div className='divider' />
+            <footer>
+              <p>
+                A passionate front-end developer dedicated to transforming ideas
+                into captivating digital realities. I bring two years of
+                hands-on experience to the table. Explore my projects showcasing
+                UI/UX design, component-based architecture, and problem-solving
+                skills.
+              </p>
+              <h3>Let's collaborate and bring your digital vision to life!</h3>
+              <div className='links'>
+                <Link to='/contact' className='contactMe'>
+                  <h2>CONTACT ME</h2>
+                </Link>
+                <Link to='/work'>
+                  <h2>View Projects</h2>
+                </Link>
+              </div>
+            </footer>
+          </section>
+          <aside>
+            <header>
+              <img src='/pfp.jpg' alt='My profile picture' />
+            </header>
+            <footer>
+              <FaSass color='#CD6799' />
+              <FaReact color='#61DBFB' />
+              <FaJs color='#F0DB4F' />
+            </footer>
+          </aside>
+        </header>
+      </div>
+    </div>
+  )
 }
 
 export default About
