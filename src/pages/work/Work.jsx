8fb0bb56537e6cec12ea5bd575f4cfcbdf1001a8
Joseph <jq84144@gmail.com>
2024-02-25 05:35:10 -0500

    Remove side projects from portfolio

--- src/pages/work/Work.jsx
+++ src/pages/work/Work.jsx
@@ -1,11 +1,9 @@
 import Card from './components/Card'
-import SideCard from './components/SideCard'
 
 import './Work.scss'
 import { FaReact, FaSass, FaJs } from 'react-icons/fa6'
 
 const MAIN_STACK = [<FaReact key={0} />, <FaSass key={1} />, <FaJs key={2} />]
-const SIDE_GHURL = 'https://github.com/jquirozz/React-side-projects'
 
 const WORKS = [
   {
@@ -13,26 +11,6 @@ const WORKS = [
     url: 'https://apexif1y.netlify.app/',
     ghUrl: 'https://github.com/jquirozz/Apexify',
     imgPath: '/work/apexify',
-    stack: MAIN_STACK
-  }
-]
-const SIDE_WORKS = [
-  {
-    name: 'To-Do App',
-    url: 'https://jqui-todo.netlify.app/',
-    ghUrl: SIDE_GHURL,
-    stack: MAIN_STACK
-  },
-  {
-    name: 'Quotes App',
-    url: 'https://jqui-quote-generator.netlify.app/',
-    ghUrl: SIDE_GHURL,
-    stack: MAIN_STACK
-  },
-  {
-    name: 'Time App',
-    url: 'https://jqui-time.netlify.app/',
-    ghUrl: SIDE_GHURL,
     stack: MAIN_STACK
   }
 ]
@@ -47,17 +25,6 @@ function Work () {
             url={w.url}
             ghUrl={w.ghUrl}
             imgPath={w.imgPath}
-            stack={w.stack}
-            key={key}
-          />
-        ))}
-      </section>
-      <section className='side'>
-        {SIDE_WORKS.map((w, key) => (
-          <SideCard
-            name={w.name}
-            url={w.url}
-            ghUrl={w.ghUrl}
             stack={w.stack}
             key={key}
           />
