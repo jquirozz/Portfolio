39ced018575fe4971848eafcdfc9668b04dae10a
jquirozz <jq84144@gmail.com>
2025-01-09 02:53:24 -0500

    Base: Portfolio + Blog

---
+++ src/pages/blog/post-1.md
@@ -0,0 +1,8 @@
+---
+title: "My First Blog Post"
+date: 2025-01-09
+author: "Joseph Quiroz"
+layout: "../../layouts/BlogLayout.astro"
+---
+
+This is a test post 👽
---
+++ src/utils/formatDate.js
@@ -0,0 +1,9 @@
+export const formatDate = (newDate) => {
+  const date = new Date(newDate);
+
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, "0");
+  const day = String(date.getDate()).padStart(2, "0");
+
+  return `${day}/${month}/${year}`;
+};
